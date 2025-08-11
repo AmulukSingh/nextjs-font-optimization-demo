767c97df131a0672f9aa336f08796f086ef42554
AmulukSingh <142198821+AmulukSingh@users.noreply.github.com>
2025-08-11 23:36:14 +0530

    Add .next/static/chunks/src_app_admin_client_[id]_page_tsx_a993527f._.js 1754935573839

---
+++ .next/static/chunks/src_app_admin_client_[id]_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_4e00e95d._.js",
+  "static/chunks/node_modules_28f0d4d9._.js"
+],
+    source: "dynamic"
+});
